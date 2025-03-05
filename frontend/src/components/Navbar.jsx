186d51d86f21ce7653f21c83d88ef6b0f559a355
mukeshy2005 <my653197@gmail.com>
2025-03-05 11:54:37 +0530

    added new files

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -67,7 +67,7 @@ const Navbar = () => {
           alt="Search Icon" 
         />
         <div className='group relative'>
-          <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="Profile Icon" />
+         <Link to='/login'> <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="Profile Icon" /> </Link>
           <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4 z-50'>
             <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
               <p className='cursor-pointer hover:text-black'>My Profile</p>
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import Title from '../components/Title'
+import NewsletterBox from '../components/NewsletterBox'
+
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+        <p>Forever was born out of a passion for innovation and a desire to revolution Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis libero eos magni veniam sunt repudiandae beatae? Aliquam nostrum nemo, sit, libero commodi veniam animi accusamus vel, iusto eius dolorem blanditiis. :</p>
+<p>Since our inception, we ve worked tirelessly to curate a diverse selection Lorem ipsum dolor sit amet consectetur, adipisicing elit. A, quasi facilis. Incidunt qui at illo neque. Vitae, molestiae voluptatum at optio nulla blanditiis alias ipsa dolor dolorum adipisci nam neque? </p>
+<b className='text-gray-800'>Our Mission</b>
+<p>Our mission at Forever is to empower customers with choice, convenience, and</p>
+        </div>
+      </div>
+ 
+      <div className='text-xl py-4'>
+  <Title text1={'WHY'} text2={'CHOOSE US'} />
+</div>
+
+<div className='flex flex-col md:flex-row text-sm mb-20'>
+  <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+    <b>Quality Assurance:</b>
+    <p className='text-grey-600'>We meticulously select and vet each product to ensure it meets our stringent(</p>
+  </div>
+  <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+    <b>Convenience:</b>
+    <p className='text-grey-600'>With our user-friendly interface and hassle-free ordering process, shopping ha</p>
+  </div>
+  <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+    <b>Exceptional Customer Service :</b>
+    <p className='text-grey-600'>With our user-friendly interface and hassle-free ordering process, shopping ha</p>
+  </div>
+</div>
+
+  <NewsletterBox/>
     </div>
   )
 }
