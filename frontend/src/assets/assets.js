e2d47448826947186bd30fafad6354566af69d3b
mukeshy2005 <my653197@gmail.com>
2025-03-05 21:19:58 +0530

    changed logo

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -62,7 +62,7 @@ import p_img52 from './p_img52.png';
 
 
 
-import logo from './logo.jpeg';
+import logo from './logo.png';
 import hero_img from './hero_img.png';
 import cart_icon from './cart_icon.png';
 import bin_icon from './bin_icon.png';
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
       <div>
         <hr />
         <p className='py-5 text-sm text-center'>
-          Copyright 2024@ kawaii.accessories.com - All Right Reserved
+          Copyright 2025@ Mukesh.Yadav - All Right Reserved
         </p>
       </div>
     </div>
